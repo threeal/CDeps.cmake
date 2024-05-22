e6664a3b357bd27b7411bc4d534e685b753ea952
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-22 20:11:21 +0700

    test: run sample project during build (#55)
    * test: execute sample project during build
    
    * test: remove `run_project` function in testing

--- test/CDepsTest.cmake
+++ test/CDepsTest.cmake
@@ -26,21 +26,9 @@ function(build_project)
   endif()
 endfunction()
 
-function(run_project)
-  message(STATUS "Running project")
-  execute_process(
-    COMMAND ${CMAKE_CURRENT_LIST_DIR}/project/build/main
-    RESULT_VARIABLE RES
-  )
-  if(NOT RES EQUAL 0)
-    message(FATAL_ERROR "Failed to run project")
-  endif()
-endfunction()
-
 function("Install missing dependencies")
   reconfigure_project()
   build_project()
-  run_project()
 endfunction()
 
 cmake_language(CALL "${TEST_COMMAND}")
