7023f06419ecbbe0d536bf19c1b33ad0e1355765
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-28 20:52:59 +0700

    chore: bump Assertion.cmake to version 0.2.0 (#58)
    * chore: bump Assertion.cmake to version 0.2.0
    
    * test: utilize `assert_execute_process` function

--- test/CDepsTest.cmake
+++ test/CDepsTest.cmake
@@ -1,34 +1,24 @@
 cmake_minimum_required(VERSION 3.5)
 
-function(reconfigure_project)
-  message(STATUS "Reconfiguring project")
-  execute_process(
+file(
+  DOWNLOAD https://threeal.github.io/assertion-cmake/v0.2.0
+    ${CMAKE_BINARY_DIR}/Assertion.cmake
+  EXPECTED_MD5 4ee0e5217b07442d1a31c46e78bb5fac
+)
+include(${CMAKE_BINARY_DIR}/Assertion.cmake)
+
+function("Install missing dependencies")
+  assert_execute_process(
     COMMAND "${CMAKE_COMMAND}"
       -B ${CMAKE_CURRENT_LIST_DIR}/project/build
       -D CMAKE_MODULE_PATH=${CMAKE_MODULE_PATH}
       --fresh
       ${CMAKE_CURRENT_LIST_DIR}/project
-    RESULT_VARIABLE RES
   )
-  if(NOT RES EQUAL 0)
-    message(FATAL_ERROR "Failed to reconfigure project")
-  endif()
-endfunction()
 
-function(build_project)
-  message(STATUS "Building project")
-  execute_process(
+  assert_execute_process(
     COMMAND "${CMAKE_COMMAND}" --build ${CMAKE_CURRENT_LIST_DIR}/project/build
-    RESULT_VARIABLE RES
   )
-  if(NOT RES EQUAL 0)
-    message(FATAL_ERROR "Failed to build project")
-  endif()
-endfunction()
-
-function("Install missing dependencies")
-  reconfigure_project()
-  build_project()
 endfunction()
 
 cmake_language(CALL "${TEST_COMMAND}")
