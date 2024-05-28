6dd4c1f9471d37fc17d1d4deffd75f76aafb3024
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-28 16:40:56 +0700

    chore: bump Assertion.cmake to version 0.2.0

--- test/CDepsParseArgsTest.cmake
+++ test/CDepsParseArgsTest.cmake
@@ -1,8 +1,9 @@
 cmake_minimum_required(VERSION 3.5)
 
 file(
-  DOWNLOAD https://threeal.github.io/assertion-cmake/v0.1.0 ${CMAKE_BINARY_DIR}/Assertion.cmake
-  EXPECTED_MD5 3c9c0dd5e971bde719d7151c673e08b4
+  DOWNLOAD https://threeal.github.io/assertion-cmake/v0.2.0
+    ${CMAKE_BINARY_DIR}/Assertion.cmake
+  EXPECTED_MD5 4ee0e5217b07442d1a31c46e78bb5fac
 )
 include(${CMAKE_BINARY_DIR}/Assertion.cmake)
 
@@ -18,26 +19,26 @@ function("Parse arguments")
       BUILD_DOCS=OFF
   )
 
-  assert_defined(ARG_NAME)
-  assert_strequal("${ARG_NAME}" Foo)
+  assert(DEFINED ARG_NAME)
+  assert(ARG_NAME STREQUAL Foo)
 
-  assert_defined(ARG_GIT_URL)
-  assert_strequal("${ARG_GIT_URL}" https://github.com/foo/foo)
+  assert(DEFINED ARG_GIT_URL)
+  assert(ARG_GIT_URL STREQUAL https://github.com/foo/foo)
 
-  assert_defined(ARG_GIT_TAG)
-  assert_strequal("${ARG_GIT_TAG}" 1.2.3)
+  assert(DEFINED ARG_GIT_TAG)
+  assert(ARG_GIT_TAG STREQUAL 1.2.3)
 
-  assert_defined(ARG_OPTIONS)
-  assert_strequal("${ARG_OPTIONS}" "BUILD_TESTING=OFF;BUILD_DOCS=OFF")
+  assert(DEFINED ARG_OPTIONS)
+  assert(ARG_OPTIONS MATCHES "^BUILD_TESTING=OFF.BUILD_DOCS=OFF$")
 endfunction()
 
 function("Parse empty arguments")
   _cdeps_parse_arguments()
 
-  assert_not_defined(ARG_NAME)
-  assert_not_defined(ARG_GIT_URL)
-  assert_not_defined(ARG_GIT_TAG)
-  assert_not_defined(ARG_OPTIONS)
+  assert(NOT DEFINED ARG_NAME)
+  assert(NOT DEFINED ARG_GIT_URL)
+  assert(NOT DEFINED ARG_GIT_TAG)
+  assert(NOT DEFINED ARG_OPTIONS)
 endfunction()
 
 cmake_language(CALL "${TEST_COMMAND}")
