552f7c3036145029ef50c9b6b2c3a13692b54081
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-24 21:10:26 +0700

    chore: bump Assertion.cmake to version 0.3.0 (#64)

--- test/CDepsTest.cmake
+++ test/CDepsTest.cmake
@@ -1,24 +1,22 @@
 cmake_minimum_required(VERSION 3.5)
 
 file(
-  DOWNLOAD https://threeal.github.io/assertion-cmake/v0.2.0
+  DOWNLOAD https://github.com/threeal/assertion-cmake/releases/download/v0.3.0/Assertion.cmake
     ${CMAKE_BINARY_DIR}/Assertion.cmake
-  EXPECTED_MD5 4ee0e5217b07442d1a31c46e78bb5fac
+  EXPECTED_MD5 851f49c10934d715df5d0b59c8b8c72a
 )
 include(${CMAKE_BINARY_DIR}/Assertion.cmake)
 
 function("Install missing dependencies")
   assert_execute_process(
-    COMMAND "${CMAKE_COMMAND}"
+    "${CMAKE_COMMAND}"
       -B ${CMAKE_CURRENT_LIST_DIR}/project/build
       -D CMAKE_MODULE_PATH=${CMAKE_MODULE_PATH}
       --fresh
-      ${CMAKE_CURRENT_LIST_DIR}/project
-  )
+      ${CMAKE_CURRENT_LIST_DIR}/project)
 
   assert_execute_process(
-    COMMAND "${CMAKE_COMMAND}" --build ${CMAKE_CURRENT_LIST_DIR}/project/build
-  )
+    "${CMAKE_COMMAND}" --build ${CMAKE_CURRENT_LIST_DIR}/project/build)
 endfunction()
 
 cmake_language(CALL "${TEST_COMMAND}")
