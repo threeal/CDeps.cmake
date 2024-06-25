12ebcc35f044a8eb7a1102b46fd9bc4a3c00da25
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-25 21:09:36 +0700

    test: expand deps installation tests to cover edge cases (#77)

--- test/DepsInstallation.cmake
+++ test/DepsInstallation.cmake
@@ -2,13 +2,95 @@ cmake_minimum_required(VERSION 3.5)
 
 include(Assertion.cmake)
 
-section("generate sample project")
+section("it should fail to download a dependency")
+  file(REMOVE_RECURSE project)
   file(MAKE_DIRECTORY project)
 
   file(
     WRITE project/CMakeLists.txt
     "cmake_minimum_required(VERSION 3.5)\n"
+    "project(Poject LANGUAGES CXX)\n"
     "\n"
+    "find_package(CDeps REQUIRED PATHS ${CMAKE_CURRENT_LIST_DIR}/../cmake)\n"
+    "cdeps_install_package(https://google.com NAME google)\n")
+
+  assert_execute_process(
+    COMMAND "${CMAKE_COMMAND}" -B project/build project
+    ERROR "CDeps: Failed to download google:")
+endsection()
+
+section("it should fail to configure a dependency")
+  file(REMOVE_RECURSE project)
+  file(MAKE_DIRECTORY project)
+
+  # TODO: Currently, a Git tag is always required.
+  file(
+    WRITE project/CMakeLists.txt
+    "cmake_minimum_required(VERSION 3.5)\n"
+    "project(Poject LANGUAGES CXX)\n"
+    "\n"
+    "find_package(CDeps REQUIRED PATHS ${CMAKE_CURRENT_LIST_DIR}/../cmake)\n"
+    "cdeps_install_package(\n"
+    "  https://github.com/threeal/project-starter\n"
+    "  NAME project-starter\n"
+    "  GIT_TAG main)\n")
+
+  assert_execute_process(
+    COMMAND "${CMAKE_COMMAND}" -B project/build project
+    ERROR "CDeps: Failed to configure project-starter:")
+endsection()
+
+section("it should fail to build a dependency")
+  file(REMOVE_RECURSE project)
+  file(MAKE_DIRECTORY project)
+
+  # TODO: Currently, a Git tag is always required.
+  file(
+    WRITE project/CMakeLists.txt
+    "cmake_minimum_required(VERSION 3.5)\n"
+    "project(Poject LANGUAGES CXX)\n"
+    "\n"
+    "find_package(CDeps REQUIRED PATHS ${CMAKE_CURRENT_LIST_DIR}/../cmake)\n"
+    "cdeps_install_package(\n"
+    "  https://github.com/threeal/cpp-starter\n"
+    "  NAME cpp-starter\n"
+    "  GIT_TAG main\n"
+    "  OPTIONS CMAKE_CXX_FLAGS=invalid CMAKE_CXX_COMPILER_WORKS=ON)\n")
+
+  assert_execute_process(
+    COMMAND "${CMAKE_COMMAND}" -B project/build project
+    ERROR "CDeps: Failed to build cpp-starter:")
+endsection()
+
+section("it should fail to install a dependency")
+  file(REMOVE_RECURSE project)
+  file(MAKE_DIRECTORY project)
+
+  # TODO: Currently, a Git tag is always required.
+  file(
+    WRITE project/CMakeLists.txt
+    "cmake_minimum_required(VERSION 3.5)\n"
+    "project(Poject LANGUAGES CXX)\n"
+    "\n"
+    "find_package(CDeps REQUIRED PATHS ${CMAKE_CURRENT_LIST_DIR}/../cmake)\n"
+    "cdeps_install_package(\n"
+    "  https://github.com/threeal/cmake-starter\n"
+    "  NAME cmake-starter\n"
+    "  GIT_TAG main\n"
+    "  OPTIONS CMAKE_SKIP_INSTALL_RULES=ON)\n")
+
+  assert_execute_process(
+    COMMAND "${CMAKE_COMMAND}" -B project/build project
+    ERROR "CDeps: Failed to install cmake-starter:")
+endsection()
+
+section("it should install a dependency")
+  file(REMOVE_RECURSE project)
+  file(MAKE_DIRECTORY project)
+
+  file(
+    WRITE project/CMakeLists.txt
+    "cmake_minimum_required(VERSION 3.5)\n"
     "project(Poject LANGUAGES CXX)\n"
     "\n"
     "find_package(FMT QUIET NO_SYSTEM_ENVIRONMENT_PATH NO_CMAKE_SYSTEM_PATH)\n"
@@ -22,8 +104,7 @@ section("generate sample project")
     "  https://github.com/fmtlib/fmt\n"
     "  NAME FMT\n"
     "  GIT_TAG 10.2.1\n"
-    "  OPTIONS FMT_MASTER_PROJECT=OFF\n"
-    ")\n"
+    "  OPTIONS FMT_MASTER_PROJECT=OFF)\n"
     "\n"
     "find_package(FMT REQUIRED NO_SYSTEM_ENVIRONMENT_PATH NO_CMAKE_SYSTEM_PATH)\n"
     "\n"
@@ -41,12 +122,7 @@ section("generate sample project")
     "  fmt::print(\"Hello world!\\n\");\n"
     "  return 0;\n"
     "}\n")
-endsection()
 
-section("reconfigure sample project")
-  assert_execute_process("${CMAKE_COMMAND}" -B project/build --fresh project)
-endsection()
-
-section("build sample project")
+  assert_execute_process("${CMAKE_COMMAND}" -B project/build project)
   assert_execute_process("${CMAKE_COMMAND}" --build project/build)
 endsection()
