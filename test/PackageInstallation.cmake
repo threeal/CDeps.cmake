119d7f856f4709cede96dc5aaab8b6cada7eca73
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-29 21:58:41 +0700

    test: separate package installation and integration tests (#104)

--- test/PackageInstallation.cmake
+++ test/PackageInstallation.cmake
@@ -14,50 +14,11 @@ section("it should fail to install an external package")
 endsection()
 
 section("it should install an external package")
-  file(REMOVE_RECURSE project)
-  file(MAKE_DIRECTORY project)
+  # TODO: Need to reset the CDeps directory.
+  file(REMOVE_RECURSE "${CDEPS_ROOT}")
 
-  file(
-    WRITE project/CMakeLists.txt
-    "cmake_minimum_required(VERSION 3.5)\n"
-    "project(Poject LANGUAGES CXX)\n"
-    "\n"
-    "include(../Assertion.cmake)\n"
-    "\n"
-    "find_package(MyFibonacci QUIET)\n"
-    "assert(NOT MyFibonacci_FOUND)\n"
-    "\n"
-    "find_package(CDeps REQUIRED PATHS ${CMAKE_CURRENT_LIST_DIR}/../cmake)\n"
-    "\n"
-    "cdeps_install_package(CppStarter github.com/threeal/cpp-starter main)\n"
-    "\n"
-    "assert(DEFINED CppStarter_INSTALL_DIR)\n"
-    "assert(EXISTS \"\${CppStarter_INSTALL_DIR}\")\n"
-    "\n"
-    "find_package(\n"
-    "  MyFibonacci REQUIRED\n"
-    "  HINTS \"\${CppStarter_INSTALL_DIR}\")\n"
-    "\n"
-    "add_executable(main main.cpp)\n"
-    "target_link_libraries(main my_fibonacci::sequence)\n"
-    "\n"
-    "add_custom_target(run_main ALL COMMAND \"$<TARGET_FILE:main>\")\n"
-    "add_dependencies(run_main main)\n")
+  cdeps_install_package(CppStarter github.com/threeal/cpp-starter main)
 
-  file(
-    WRITE project/main.cpp
-    "#include <my_fibonacci/sequence.hpp>\n"
-    "#include <iostream>\n"
-    "\n"
-    "int main() {\n"
-    "  const auto sequence = my_fibonacci::fibonacci_sequence(5);\n"
-    "  for (auto val : sequence) {\n"
-    "    std::cout << val << \" \";\n"
-    "  }\n"
-    "  std::cout << std::endl;\n"
-    "  return 0;\n"
-    "}\n")
-
-  assert_execute_process("${CMAKE_COMMAND}" -B project/build project)
-  assert_execute_process("${CMAKE_COMMAND}" --build project/build)
+  assert(DEFINED CppStarter_INSTALL_DIR)
+  assert(EXISTS "${CppStarter_INSTALL_DIR}")
 endsection()
