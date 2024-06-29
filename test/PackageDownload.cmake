002b4642307fbcc97d1454c899b754786ce96215
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-29 11:24:32 +0700

    feat: remove package directory on failure  (#92)
    * feat: remove package directories on failure
    
    * test: minimize test from removing package directories

--- test/PackageDownload.cmake
+++ test/PackageDownload.cmake
@@ -4,18 +4,15 @@ find_package(CDeps REQUIRED PATHS ${CMAKE_CURRENT_LIST_DIR}/../cmake)
 include(Assertion.cmake)
 
 set(CDEPS_ROOT ${CMAKE_CURRENT_BINARY_DIR}/.cdeps)
+file(REMOVE_RECURSE "${CDEPS_ROOT}")
 
 section("it should fail to download the source code of an external package")
-  file(REMOVE_RECURSE "${CDEPS_ROOT}")
-
   assert_fatal_error(
     CALL cdeps_download_package google.com NAME google
     MESSAGE "CDeps: Failed to download google:")
 endsection()
 
 section("it should download the source code of an external package")
-  file(REMOVE_RECURSE "${CDEPS_ROOT}")
-
   # TODO: Currently, a Git tag is always required.
   cdeps_download_package(
     github.com/threeal/project-starter
