e0a77654192f851336d5165749a165ebc0cfe0a4
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-30 18:33:40 +0700

    feat: add `RECURSE_SUBMODULES` option to the `cdeps_download_package` function
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- test/cdeps_download_package.cmake
+++ test/cdeps_download_package.cmake
@@ -102,3 +102,74 @@ section("it should not redownload an external package")
       OUTPUT "1")
   endsection()
 endsection()
+
+section("it should download an external package without submodules")
+  cdeps_download_package(
+    GitSubmoduleExample github.com/threeal/git-submodule-example v1.0.0)
+
+  section("it should download to the correct path")
+    assert(DEFINED GitSubmoduleExample_SOURCE_DIR)
+    assert(EXISTS "${GitSubmoduleExample_SOURCE_DIR}")
+
+    cdeps_get_package_dir(GitSubmoduleExample PACKAGE_DIR)
+    assert(GitSubmoduleExample_SOURCE_DIR STREQUAL "${PACKAGE_DIR}/src")
+  endsection()
+
+  section("it should download the correct version")
+    find_package(Git REQUIRED)
+    assert_execute_process(
+      COMMAND ${GIT_EXECUTABLE} -C "${PACKAGE_DIR}/src" rev-parse HEAD
+      OUTPUT "9264df25ee6d43751d8948b746e808cf6069f834")
+  endsection()
+
+  section("it should download only the latest change")
+    find_package(Git REQUIRED)
+    assert_execute_process(
+      COMMAND ${GIT_EXECUTABLE} -C "${PACKAGE_DIR}/src" rev-list --count HEAD
+      OUTPUT "1")
+  endsection()
+
+  section("it should not download the submodules")
+    find_package(Git REQUIRED)
+    assert_execute_process(
+      COMMAND ${GIT_EXECUTABLE} -C "${PACKAGE_DIR}/src" submodule status
+        --recursive
+      OUTPUT "-bea29a84b6c55a49fef34be3e7a17498c633b6d9 project-starter")
+  endsection()
+endsection()
+
+section("it should redownload an external package with submodules")
+  cdeps_download_package(
+    GitSubmoduleExample github.com/threeal/git-submodule-example v1.0.0
+    RECURSE_SUBMODULES)
+
+  section("it should redownload to the correct path")
+    assert(DEFINED GitSubmoduleExample_SOURCE_DIR)
+    assert(EXISTS "${GitSubmoduleExample_SOURCE_DIR}")
+
+    cdeps_get_package_dir(GitSubmoduleExample PACKAGE_DIR)
+    assert(GitSubmoduleExample_SOURCE_DIR STREQUAL "${PACKAGE_DIR}/src")
+  endsection()
+
+  section("it should redownload the correct version")
+    find_package(Git REQUIRED)
+    assert_execute_process(
+      COMMAND ${GIT_EXECUTABLE} -C "${PACKAGE_DIR}/src" rev-parse HEAD
+      OUTPUT "9264df25ee6d43751d8948b746e808cf6069f834")
+  endsection()
+
+  section("it should redownload only the latest change")
+    find_package(Git REQUIRED)
+    assert_execute_process(
+      COMMAND ${GIT_EXECUTABLE} -C "${PACKAGE_DIR}/src" rev-list --count HEAD
+      OUTPUT "1")
+  endsection()
+
+  section("it should redownload the submodules")
+    find_package(Git REQUIRED)
+    assert_execute_process(
+      COMMAND ${GIT_EXECUTABLE} -C "${PACKAGE_DIR}/src" submodule status
+        --recursive
+      OUTPUT "bea29a84b6c55a49fef34be3e7a17498c633b6d9 project-starter \\(v1\\.2\\.0\\)")
+  endsection()
+endsection()
