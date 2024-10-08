570966a4b43deb9913b5bd6bdf2c0f00cb728912
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-08 21:51:40 +0700

    test: refactor `cdeps_build_package.cmake` test file
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- test/cdeps_build_package.cmake
+++ test/cdeps_build_package.cmake
@@ -1,172 +1,122 @@
 include(${CMAKE_CURRENT_LIST_DIR}/../cmake/CDeps.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/assert_helper.cmake)
+
+set(CMAKE_GENERATOR "Unix Makefiles")
 
 set(CDEPS_ROOT ${CMAKE_CURRENT_BINARY_DIR}/.cdeps)
 file(REMOVE_RECURSE "${CDEPS_ROOT}")
 
-cdeps_get_package_dir(Sample SAMPLE_PACKAGE_DIR)
+cdeps_get_package_dir(Sample PACKAGE_DIR)
+set(Sample_SOURCE_DIR ${PACKAGE_DIR}/src)
 
-section(
-  "it should fail to build an external package that has not been downloaded")
+section("it should fail to build an external package "
+  "that has not been downloaded")
   assert_fatal_error(
     CALL cdeps_build_package Sample
     MESSAGE "CDeps: Sample must be downloaded before building")
 endsection()
 
-function(test_generate_and_build_external_package)
-  section("it should generate external package source files")
-    file(
-      WRITE ${SAMPLE_PACKAGE_DIR}/src/CMakeLists.txt
-      "cmake_minimum_required(VERSION 3.5)\n"
-      "project(Sample LANGUAGES CXX)\n"
-      "\n"
-      "option(BUILD_MARS \"\" OFF)\n"
-      "\n"
-      "add_executable(earth earth.cpp)\n"
-      "install(TARGETS earth EXPORT earth_targets)\n"
-      "\n"
-      "if(BUILD_MARS)\n"
-      "  add_executable(mars mars.cpp)\n"
-      "  install(TARGETS mars EXPORT mars_targets)\n"
-      "endif()\n")
+section("it should generate external package source files")
+  file(WRITE ${Sample_SOURCE_DIR}/CMakeLists.txt
+    "cmake_minimum_required(VERSION 3.5)\n"
+    "project(Sample LANGUAGES CXX)\n"
+    "\n"
+    "option(BUILD_MARS \"\" OFF)\n"
+    "\n"
+    "add_executable(earth earth.cpp)\n"
+    "install(TARGETS earth EXPORT earth_targets)\n"
+    "\n"
+    "if(BUILD_MARS)\n"
+    "  add_executable(mars mars.cpp)\n"
+    "  install(TARGETS mars EXPORT mars_targets)\n"
+    "endif()\n")
 
-    file(
-      WRITE ${SAMPLE_PACKAGE_DIR}/src/earth.cpp
-      "#include <iostream>\n"
-      "\n"
-      "int main() {\n"
-      "  std::cout << \"Hello Earth!\\n\";\n"
-      "}\n")
+  file(WRITE ${Sample_SOURCE_DIR}/earth.cpp
+    "#include <iostream>\n"
+    "\n"
+    "int main() {\n"
+    "  std::cout << \"Hello Earth!\\n\";\n"
+    "}\n")
 
-    file(
-      WRITE ${SAMPLE_PACKAGE_DIR}/src/mars.cpp
-      "#include <iostream>\n"
-      "\n"
-      "int main() {\n"
-      "  std::cout << \"Hello Mars!\\n\";\n"
-      "}\n")
+  file(WRITE ${Sample_SOURCE_DIR}/mars.cpp
+    "#include <iostream>\n"
+    "\n"
+    "int main() {\n"
+    "  std::cout << \"Hello Mars!\\n\";\n"
+    "}\n")
 
-    file(
-      WRITE ${SAMPLE_PACKAGE_DIR}/src.lock
-      "Sample github.com/user/sample main")
-  endsection()
-
-  section("it should build an external package")
-    cdeps_build_package(Sample)
-
-    section("it should build in the correct path")
-      assert(DEFINED Sample_BUILD_DIR)
-      assert(EXISTS "${Sample_BUILD_DIR}")
-
-      assert(Sample_BUILD_DIR STREQUAL ${SAMPLE_PACKAGE_DIR}/build)
-    endsection()
-
-    section("it should use the correct build system generator")
-      assert(NOT EXISTS ${Sample_BUILD_DIR}/build.ninja)
-    endsection()
-
-    section("it should build the correct targets")
-      assert(EXISTS ${SAMPLE_PACKAGE_DIR}/build/earth)
-      assert_execute_process(COMMAND ${SAMPLE_PACKAGE_DIR}/build/earth)
-
-      assert(NOT EXISTS ${SAMPLE_PACKAGE_DIR}/build/mars)
-    endsection()
-  endsection()
-endfunction()
-
-test_generate_and_build_external_package()
-
-section("it should rebuild an external package with the same generator as the"
-  " parent project")
-
-  set(CMAKE_GENERATOR Ninja)
-
-  cdeps_build_package(Sample)
-
-  unset(CMAKE_GENERATOR)
-
-  section("it should rebuild in the correct path")
-    assert(DEFINED Sample_BUILD_DIR)
-    assert(EXISTS "${Sample_BUILD_DIR}")
-
-    assert(Sample_BUILD_DIR STREQUAL ${SAMPLE_PACKAGE_DIR}/build)
-  endsection()
-
-  section("it should use the correct build system generator")
-    assert(EXISTS ${Sample_BUILD_DIR}/build.ninja)
-  endsection()
-
-  section("it should rebuild the correct targets")
-    assert(EXISTS ${SAMPLE_PACKAGE_DIR}/build/earth)
-    assert_execute_process(COMMAND ${SAMPLE_PACKAGE_DIR}/build/earth)
-
-    assert(NOT EXISTS ${SAMPLE_PACKAGE_DIR}/build/mars)
-  endsection()
-endsection()
-
-section("it should rebuild an external package with a different options")
-  cdeps_build_package(Sample OPTIONS BUILD_MARS=ON)
-
-  section("it should rebuild in the correct path")
-    assert(DEFINED Sample_BUILD_DIR)
-    assert(EXISTS "${Sample_BUILD_DIR}")
-
-    assert(Sample_BUILD_DIR STREQUAL ${SAMPLE_PACKAGE_DIR}/build)
-  endsection()
-
-  section("it should use the correct build system generator")
-    assert(NOT EXISTS ${Sample_BUILD_DIR}/build.ninja)
-  endsection()
-
-  section("it should rebuild the correct targets")
-    assert(EXISTS ${SAMPLE_PACKAGE_DIR}/build/earth)
-    assert_execute_process(COMMAND ${SAMPLE_PACKAGE_DIR}/build/earth)
-
-    assert(EXISTS ${SAMPLE_PACKAGE_DIR}/build/mars)
-    assert_execute_process(COMMAND ${SAMPLE_PACKAGE_DIR}/build/mars)
-  endsection()
-endsection()
-
-section("it should rebuild an external package with a different generator")
-  cdeps_build_package(Sample GENERATOR Ninja)
-
-  section("it should rebuild in the correct path")
-    assert(DEFINED Sample_BUILD_DIR)
-    assert(EXISTS "${Sample_BUILD_DIR}")
-
-    assert(Sample_BUILD_DIR STREQUAL ${SAMPLE_PACKAGE_DIR}/build)
-  endsection()
-
-  section("it should use the correct build system generator")
-    assert(EXISTS ${Sample_BUILD_DIR}/build.ninja)
-  endsection()
-
-  section("it should rebuild the correct targets")
-    assert(EXISTS ${SAMPLE_PACKAGE_DIR}/build/earth)
-    assert_execute_process(COMMAND ${SAMPLE_PACKAGE_DIR}/build/earth)
-
-    assert(NOT EXISTS ${SAMPLE_PACKAGE_DIR}/build/mars)
-  endsection()
-endsection()
-
-section("it should fail to build a corrupted external package")
-  file(WRITE ${SAMPLE_PACKAGE_DIR}/src/earth.cpp corrupted)
-  file(WRITE ${SAMPLE_PACKAGE_DIR}/src.lock corrupted)
-
-  assert_fatal_error(
-    CALL cdeps_build_package Sample
-    MESSAGE "CDeps: Failed to build Sample:")
+  file(WRITE ${Sample_SOURCE_DIR}.lock
+    "Sample github.com/user/sample main")
 endsection()
 
 section("it should fail to configure a corrupted external package")
-  file(WRITE ${SAMPLE_PACKAGE_DIR}/src/CMakeLists.txt corrupted)
-  file(WRITE ${SAMPLE_PACKAGE_DIR}/src.lock corrupted)
+  file(READ ${Sample_SOURCE_DIR}/CMakeLists.txt ORIGINAL_CMAKELISTS_TXT)
+  file(WRITE ${Sample_SOURCE_DIR}/CMakeLists.txt corrupted)
 
   assert_fatal_error(
     CALL cdeps_build_package Sample
     MESSAGE "CDeps: Failed to configure Sample:")
+
+  file(WRITE ${Sample_SOURCE_DIR}/CMakeLists.txt "${ORIGINAL_CMAKELISTS_TXT}")
 endsection()
 
-test_generate_and_build_external_package()
+section("it should fail to build a corrupted external package")
+  file(READ ${Sample_SOURCE_DIR}/earth.cpp ORIGINAL_EARTH_CPP)
+  file(WRITE ${Sample_SOURCE_DIR}/earth.cpp corrupted)
+
+  assert_fatal_error(
+    CALL cdeps_build_package Sample
+    MESSAGE "CDeps: Failed to build Sample:")
+
+  file(WRITE ${Sample_SOURCE_DIR}/earth.cpp "${ORIGINAL_EARTH_CPP}")
+endsection()
+
+section("it should build an external package with a default generator")
+  set(PREV_CMAKE_GENERATOR "${CMAKE_GENERATOR}")
+  unset(CMAKE_GENERATOR)
+
+  cdeps_build_package(Sample)
+
+  section("it should build in the correct path")
+    assert_cdeps_package_build_dir(Sample)
+  endsection()
+
+  section("it should build the correct targets")
+    assert(EXISTS ${Sample_BUILD_DIR}/earth)
+    assert_execute_process(COMMAND ${Sample_BUILD_DIR}/earth)
+
+    assert(NOT EXISTS ${Sample_BUILD_DIR}/mars)
+  endsection()
+
+  set(CMAKE_GENERATOR "${PREV_CMAKE_GENERATOR}")
+endsection()
+
+section("it should fail to rebuild an external package "
+  "with an invalid parent project's generator")
+  set(PREV_CMAKE_GENERATOR "${CMAKE_GENERATOR}")
+  set(CMAKE_GENERATOR invalid)
+
+  assert_fatal_error(
+    CALL cdeps_build_package Sample
+    MESSAGE "CDeps: Failed to configure Sample:")
+
+  set(CMAKE_GENERATOR "${PREV_CMAKE_GENERATOR}")
+endsection()
+
+section("it should build an external package")
+  cdeps_build_package(Sample)
+
+  section("it should build in the correct path")
+    assert_cdeps_package_build_dir(Sample)
+  endsection()
+
+  section("it should build the correct targets")
+    assert(EXISTS ${Sample_BUILD_DIR}/earth)
+    assert_execute_process(COMMAND ${Sample_BUILD_DIR}/earth)
+
+    assert(NOT EXISTS ${Sample_BUILD_DIR}/mars)
+  endsection()
+endsection()
 
 section("it should not rebuild an external package")
   set(PREV_CMAKE_COMMAND "${CMAKE_COMMAND}")
@@ -177,20 +127,49 @@ section("it should not rebuild an external package")
   set(CMAKE_COMMAND "${PREV_CMAKE_COMMAND}")
 
   section("it should maintain the correct path")
-    assert(DEFINED Sample_BUILD_DIR)
-    assert(EXISTS "${Sample_BUILD_DIR}")
-
-    assert(Sample_BUILD_DIR STREQUAL ${SAMPLE_PACKAGE_DIR}/build)
-  endsection()
-
-  section("it should maintain the correct build system generator")
-    assert(NOT EXISTS ${Sample_BUILD_DIR}/build.ninja)
-  endsection()
-
-  section("it should maintain the build targets")
-    assert(EXISTS ${SAMPLE_PACKAGE_DIR}/build/earth)
-    assert_execute_process(COMMAND ${SAMPLE_PACKAGE_DIR}/build/earth)
-
-    assert(NOT EXISTS ${SAMPLE_PACKAGE_DIR}/build/mars)
+    assert_cdeps_package_build_dir(Sample)
   endsection()
 endsection()
+
+section("it should rebuild an external package with a different options")
+  cdeps_build_package(Sample OPTIONS BUILD_MARS=ON)
+
+  section("it should rebuild in the correct path")
+    assert_cdeps_package_build_dir(Sample)
+  endsection()
+
+  section("it should rebuild the correct targets")
+    assert(EXISTS ${Sample_BUILD_DIR}/earth)
+    assert_execute_process(COMMAND ${Sample_BUILD_DIR}/earth)
+
+    assert(EXISTS ${Sample_BUILD_DIR}/mars)
+    assert_execute_process(COMMAND ${Sample_BUILD_DIR}/mars)
+  endsection()
+endsection()
+
+section("it should fail to rebuild an external package with an invalid generator")
+  assert_fatal_error(
+    CALL cdeps_build_package Sample GENERATOR invalid
+    MESSAGE "CDeps: Failed to configure Sample:")
+endsection()
+
+section("it should build an external package with a valid generator")
+  cdeps_build_package(Sample GENERATOR "Unix Makefiles")
+
+  section("it should build in the correct path")
+    assert_cdeps_package_build_dir(Sample)
+  endsection()
+
+  section("it should use the correct build system generator")
+    assert(EXISTS ${Sample_BUILD_DIR}/Makefile)
+  endsection()
+
+  section("it should build the correct targets")
+    assert(EXISTS ${Sample_BUILD_DIR}/earth)
+    assert_execute_process(COMMAND ${Sample_BUILD_DIR}/earth)
+
+    assert(NOT EXISTS ${Sample_BUILD_DIR}/mars)
+  endsection()
+endsection()
+
+file(REMOVE_RECURSE "${CDEPS_ROOT}")
