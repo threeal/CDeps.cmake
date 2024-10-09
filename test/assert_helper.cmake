5d296d6a15139951dc4403fa903dd8534f2d8562
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-09 20:01:38 +0700

    test: split `test_build_package.cmake` into several files
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- test/assert_helper.cmake
+++ test/assert_helper.cmake
@@ -1,17 +1,3 @@
-# Asserts that the build directory of the specified package is valid.
-#
-# assert_cdeps_package_build_dir(<name>)
-#
-# This function checks whether the `<name>_BUILD_DIR` variable is defined and
-# points to an existing and valid build directory.
-function(assert_cdeps_package_build_dir NAME)
-  assert(DEFINED ${NAME}_BUILD_DIR)
-  assert(EXISTS "${${NAME}_BUILD_DIR}")
-
-  cdeps_get_package_dir("${NAME}" PACKAGE_DIR)
-  assert(${NAME}_BUILD_DIR STREQUAL "${PACKAGE_DIR}/build")
-endfunction()
-
 # Asserts that the install directory of the specified package is valid.
 #
 # assert_cdeps_package_install_dir(<name>)
