1fd2b34122e827e14711370807f1342babaf1eae
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-12 16:23:24 +0700

    feat: add support for downloading package using commit hash as the reference (#170)
    * test: modify `test_download_version.cmake` to test download with tag and branch
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    * feat: modify commands to download external package
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    * test: modify `test_download_version.cmake` to test download with commit hash
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    ---------
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- test/test_download_version.cmake
+++ test/test_download_version.cmake
@@ -5,15 +5,30 @@ find_package(Git REQUIRED QUIET)
 set(CDEPS_DIR .cdeps)
 file(REMOVE_RECURSE .cdeps)
 
-section("it should download a package with a specific version")
-  cdeps_download_package(pkg github.com/threeal/project-starter v1.0.0)
+section("it should download a package with a specific branch name")
+  cdeps_download_package(pkg github.com/threeal/project-starter main)
 
   section("it should generate the lock file")
+    file(READ .cdeps/pkg/src.lock CONTENT)
+    assert(CONTENT MATCHES main$)
+  endsection()
+
+  section("it should download with the correct branch name")
+    assert_execute_process(
+      COMMAND ${GIT_EXECUTABLE} -C .cdeps/pkg/src rev-parse --abbrev-ref HEAD
+      OUTPUT main)
+  endsection()
+endsection()
+
+section("it should redownload the package with a specific tag name")
+  cdeps_download_package(pkg github.com/threeal/project-starter v1.0.0)
+
+  section("it should regenerate the lock file")
     file(READ .cdeps/pkg/src.lock CONTENT)
     assert(CONTENT MATCHES v1.0.0$)
   endsection()
 
-  section("it should download with the correct version")
+  section("it should redownload with the correct tag name")
     assert_execute_process(
       COMMAND ${GIT_EXECUTABLE} -C .cdeps/pkg/src
         describe --exact-match --tags v1.0.0
@@ -21,19 +36,19 @@ section("it should download a package with a specific version")
   endsection()
 endsection()
 
-section("it should redownload the package with a different version")
-  cdeps_download_package(pkg github.com/threeal/project-starter v1.1.0)
+section("it should redownload the package with a specific commit hash")
+  cdeps_download_package(pkg github.com/threeal/project-starter
+    ad2c03959cb67fffc7cce7266bc244c73a2af8ec)
 
   section("it should regenerate the lock file")
     file(READ .cdeps/pkg/src.lock CONTENT)
-    assert(CONTENT MATCHES v1.1.0$)
+    assert(CONTENT MATCHES ad2c03959cb67fffc7cce7266bc244c73a2af8ec$)
   endsection()
 
-  section("it should redownload with the correct version")
+  section("it should redownload with the correct commit hash")
     assert_execute_process(
-      COMMAND ${GIT_EXECUTABLE} -C .cdeps/pkg/src
-        describe --exact-match --tags v1.1.0
-      OUTPUT v1.1.0)
+      COMMAND ${GIT_EXECUTABLE} -C .cdeps/pkg/src rev-parse HEAD
+      OUTPUT ad2c03959cb67fffc7cce7266bc244c73a2af8ec)
   endsection()
 endsection()
 
